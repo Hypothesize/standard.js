937ee21d8f5739cb3c30b6beaf6ad07bc9bb1c84
Diego Cisneros <diegocl02@gmail.com>
2021-03-01 20:19:11 -0500

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
 		//"fp/no-class": "warn",
 		"fp/no-delete": "warn",
 		"fp/no-events": "warn",
-		"fp/no-get-set": "warn",
+		// "fp/no-get-set": "warn",
 		"fp/no-let": "warn",
 		"fp/no-loops": "warn",
 		"fp/no-mutating-assign": "warn",
