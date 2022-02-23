762650bd275111a69132372496c75dae60e70490
eltharim <arthur.plouet@gmail.com>
2022-02-23 16:01:38 +0800

    Update

--- test/statistical.test.ts
+++ test/statistical.test.ts
@@ -88,7 +88,7 @@ describe('median', function () {
 		assert.deepStrictEqual(actual, expected)
 	})
 
-	it('should find the median of an array of, sorted alphabetically', function () {
+	it('should find the median of an array of numbers, sorted alphabetically', function () {
 		const actual = median([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
 		const expected = 18
 
